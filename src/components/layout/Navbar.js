a33b30a9c7b12a60cc8ded402b6187944dca38a1
Roger Earl <rogerearl@me.com>
2023-02-27 20:47:25 -0800

    Added title and image to home page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,15 +4,15 @@ import { NavLink } from "react-router-dom";
 const navs = [
     { path: '/', name: 'Home' },
     { path: '/projects', name: 'Projects' },
-    { path: '/pics', name: 'Pics' },
     { path: '/about', name: 'About' },
-    { path: '/contact', name: 'Contact' }
+    { path: '/contact', name: 'Contact' },
+    { path: '/pics', name: 'Pics of my dog' }
 ]
 
 export default function Navbar() {
     return (
         <nav  className="bg-gray-600 text-blue-300 shadow p-5">
-            <ul className="flex space-x-4 justify-center">
+            <ul className="flex space-x-28 justify-center">
                 {navs.map(navItem => (
                     <li><NavLink to={navItem.path} className={({ isActive }) => isActive ? "border-b-2 border-blue-100 text-blue-100" : undefined}>{navItem.name}</NavLink></li>
                 ))}
